1d062ce88d209a17efefaa34898de74f85eefa64
Steve Kellock <steve@kellock.ca>
2018-03-03 08:06:57 -0500

    ✨ Excludes node_modules from being listed in tree mode

--- fish/functions/lt.fish
+++ fish/functions/lt.fish
@@ -1,4 +1,4 @@
 function lt --description 'Lists all files in a tree.'
-  exa -hlmg --color-scale --group-directories-first --git -T -L 3 $argv
+  exa -hlmg --color-scale --group-directories-first --git -T -L 3 -I node_modules $argv
 end
 
