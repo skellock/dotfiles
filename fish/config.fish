c1bf1669e8770605f66acf8d725b782fa1a71678
Steve Kellock <steve@kellock.ca>
2019-01-16 10:15:38 -0500

    removes unused crap

--- fish/config.fish
+++ fish/config.fish
@@ -5,7 +5,7 @@ set -x PAGER "less"
 set -x LANG "en_US.UTF-8"
 set -x ANDROID_HOME $HOME/Library/Android/sdk
 set -x FASTLANE_USER steve@kellock.ca
-set -x FZF_DEFAULT_COMMAND "fd --type f --follow --hidden --exclude .git"
+set -x FZF_DEFAULT_COMMAND "fd --type f --follow --hidden --exclude .git --exclude node_modules"
 
 # path
 set -x PATH \
@@ -21,10 +21,6 @@ end
 
 if test -e $HOME/go/bin
   set -x PATH $PATH $HOME/go/bin
-end
-
-if test -e $HOME/.opam/system/bin
-  set -x PATH $PATH $HOME/.opam/system/bin
 end
 
 # add flutter to the path
@@ -56,12 +52,12 @@ abbr gcom "git checkout master"
 abbr gd "git diff"
 abbr gdc "git diff --cached"
 abbr gpl "git pull"
-abbr gplr "git pull --rebase"
 abbr gup "git pull --rebase"
 abbr gp "git push"
 abbr gpoh "git push -u origin HEAD"
 abbr gpristine "git reset --hard; git clean -dfx"
 abbr gs "git status -s"
+
 abbr be "bundle exec"
 abbr bef "bundle exec fastlane"
 abbr fs "forego start"
@@ -76,25 +72,29 @@ source ~/.config/fish/functions/rprompt.fish
 # swift support
 if which swiftenv > /dev/null 2> /dev/null; status --is-interactive; and source (swiftenv init -|psub); end
 
+# ocaml
+#if test -e $HOME/.opam/system/bin
+#  set -x PATH $PATH $HOME/.opam/system/bin
+#end
+
 # reason support
 # . /Users/steve/.opam/opam-init/init.fish > /dev/null 2> /dev/null or true
 # set -gx PATH "/Users/steve/.opam/system/bin" "$PATH";
-set -gx OCAML_TOPLEVEL_PATH "/Users/steve/.opam/system/lib/toplevel";
-set -gx PERL5LIB "/Users/steve/.opam/system/lib/perl5:$PERL5LIB";
-set -gx MANPATH "$MANPATH" "/Users/steve/.opam/system/man";
-set -gx OPAMUTF8MSGS "1";
-set -gx CAML_LD_LIBRARY_PATH "/Users/steve/.opam/system/lib/stublibs:/usr/local/lib/ocaml/stublibs";
+# set -gx OCAML_TOPLEVEL_PATH "/Users/steve/.opam/system/lib/toplevel";
+# set -gx PERL5LIB "/Users/steve/.opam/system/lib/perl5:$PERL5LIB";
+# set -gx MANPATH "$MANPATH" "/Users/steve/.opam/system/man";
+# set -gx OPAMUTF8MSGS "1";
+# set -gx CAML_LD_LIBRARY_PATH "/Users/steve/.opam/system/lib/stublibs:/usr/local/lib/ocaml/stublibs";
 
 # crystal support
-if test -e "/usr/local/opt/openssl/lib/pkgconfig"
-  set -U -x PKG_CONFIG_PATH /usr/local/opt/openssl/lib/pkgconfig
-end
+#if test -e "/usr/local/opt/openssl/lib/pkgconfig"
+#  set -U -x PKG_CONFIG_PATH /usr/local/opt/openssl/lib/pkgconfig
+#end
 
 # secrets
 if test -e "$HOME/.secrets"
   source $HOME/.secrets
 end
-
 
 # fzf
 if test -e "$HOME/.fzf/shell/key-bindings.fish"
