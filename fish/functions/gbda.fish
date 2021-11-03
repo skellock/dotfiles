3c2e4a9dc290b42b35f6b773c3595c74b427bec1
Steve Kellock <steve@kellock.ca>
2021-11-03 14:16:52 -0400

    includes main as a preferred repo name

--- fish/functions/gbda.fish
+++ fish/functions/gbda.fish
@@ -1,3 +1,3 @@
 function gbda --description 'deletes all merged branches'
-  git branch --no-color --merged | grep -vE "^(\*|\s*(master|develop|dev)\s*\$)" | xargs -n 1 git branch -d
+  git branch --no-color --merged | grep -vE "^(\*|\s*(master|main|develop|dev)\s*\$)" | xargs -n 1 git branch -d
 end
